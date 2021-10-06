6e11e3e143f0c99d20eaa9c3f2cd4ee3df8d2cef
liveasy97 <82060459+liveasy97@users.noreply.github.com>
2021-10-06 22:15:18 +0530

    Update Controller.java

--- src/main/java/com/spring/gpsApiData/controller/Controller.java
+++ src/main/java/com/spring/gpsApiData/controller/Controller.java
@@ -1,14 +1,11 @@
 package com.spring.gpsApiData.controller;
-
 import java.util.List;
 import java.util.stream.Stream;
-
 import com.spring.gpsApiData.entities.historyData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.spring.gpsApiData.service.GpsDataService;
 
 @RestController
